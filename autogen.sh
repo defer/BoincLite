6d544ae5afeefba7f58634c034e6f71bf636b775
Diogo Ferreira <deovferreira@gmail.com>
2011-03-05 12:46:17 +0000

    darwin: use glibtoolize if present

--- autogen.sh
+++ autogen.sh
@@ -2,6 +2,10 @@
 autoscan 2>&1 | grep -v cellos
 autoheader
 aclocal
-libtoolize -f
+if [ -x /usr/bin/glibtoolize ]; then
+    glibtoolize -f
+else
+    libtoolize -f
+fi
 autoconf
 automake -a -Wall
